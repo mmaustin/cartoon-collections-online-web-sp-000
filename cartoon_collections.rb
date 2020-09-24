4e34156f17a9c06177c3ca866ed42a0300af99f6
mccray austin <tubach75@hotmail.com>
2020-09-24 14:20:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,10 @@ end
 summon_captain_planet(["carrot", "cucumber", "pepper"])
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
 end
+long_planeteer_calls(["axe", "earth", "wind", "fire"])
 
 def find_the_cheese# code an argument here
   # the array below is here to help
